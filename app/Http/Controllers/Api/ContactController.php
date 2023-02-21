583ff2e9142ddbda784b96a32ddb5dcbd29579f0
gab-che <gabriellachessa1@gmail.com>
2023-02-21 16:07:47 +0100

    stores form data in db

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ContactController extends Controller
 {
     public function store(Request $request)
     {
-        dump($request);
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'title' => 'required|string|max:255',
+            'message' => 'required|string',
+            'attachment' => 'nullable|file',
+        ]);
+
+        if (!is_null($data['attachment'])) {
+            $data['attachment'] = Storage::put('/contacts', $data['attachment']);
+        }
+
+        $newContact = Contact::create($data);
+        return response()->json($newContact);
     }
 }
